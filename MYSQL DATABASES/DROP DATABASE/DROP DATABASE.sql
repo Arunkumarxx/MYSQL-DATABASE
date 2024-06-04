8e0994f600496b63846bfb257f73197628fee57e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-04 17:56:36 +0530

    modified

--- MYSQL DATABASES/DROP DATABASE/DROP DATABASE.sql
+++ MYSQL DATABASES/DROP DATABASE/DROP DATABASE.sql
@@ -4,3 +4,26 @@ create database Three;
 create database Four;
 
 show databases ;
+/*
+ mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| employees          |
+| four               |
+| information_schema |
+| mysql              |
+| one                |
+| performance_schema |
+| personal_database  |
+| personaldb         |
+| personnelschema    |
+| sakila             |
+| sys                |
+| testdatabase       |
+| three              |
+| two                |
+| world              |
++--------------------+
+15 rows in set (0.00 sec)
+ */
