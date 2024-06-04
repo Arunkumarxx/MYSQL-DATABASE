724dd5a8360c5751bc0910d49b8b6be9aa52e8aa
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-04 18:53:12 +0530

    modified

--- MYSQL DATABASES/DATABASE EXPORT/DATABASE EXPORT.sql
+++ MYSQL DATABASES/DATABASE EXPORT/DATABASE EXPORT.sql
@@ -8,5 +8,11 @@ create table temp
 insert into temp values(32);
 
 /*
+C:\Program Files\MySQL\MySQL Server 8.0\bin>mysqldump -u Arunkumarxx -p personal_database > "F:\GOOGLE DRIVE\ARCHIVE HUB\personal_databaseBackup.sql"
+Enter password: *********
 
+C:\Program Files\MySQL\MySQL Server 8.0\bin>mysqldump -u Arunkumarxx -p --all-databases > "F:\GOOGLE DRIVE\ARCHIVE HUB\all_databases.sql"
+Enter password: *********
+
+C:\Program Files\MySQL\MySQL Server 8.0\bin>
  */
