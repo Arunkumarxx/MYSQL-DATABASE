238e6f1a0b96216f31b5b507d20d670114c104fc
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-04 18:35:04 +0530

    modified

--- MYSQL DATABASES/COPY DATABASE/COPY DATABASE.sql
+++ MYSQL DATABASES/COPY DATABASE/COPY DATABASE.sql
@@ -7,4 +7,10 @@ CREATE TABLE TempDatabse AS SELECT * FROM personal_database.customers;
 
 /*
  mysqldump -u [username] -p [database_name] > [output_file.sql]
+
+ C:\Program Files\MySQL\MySQL Server 8.0\bin>mysqldump -u Arunkumarxx -p personal_database > "F:\GOOGLE DRIVE\ARCHIVE HUB\personal_databaseBackup.sql"
+Enter password: *********
+
+C:\Program Files\MySQL\MySQL Server 8.0\bin>
+
  */
