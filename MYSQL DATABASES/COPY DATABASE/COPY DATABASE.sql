e4b4a5b952e29fe7927754b075e2420bee05678d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-04 18:34:08 +0530

    modified

--- MYSQL DATABASES/COPY DATABASE/COPY DATABASE.sql
+++ MYSQL DATABASES/COPY DATABASE/COPY DATABASE.sql
@@ -2,4 +2,4 @@
 CREATE DATABASE TempDatabse;
 -- Copy tables from the source database to the new database
 USE personal_database;
-CREATE TABLE new_database.table_name AS SELECT * FROM personal_database.table_name;
+CREATE TABLE TempDatabse. AS SELECT * FROM personal_database.customers;
