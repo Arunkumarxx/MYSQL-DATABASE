446d51fabba67e67cd3076646b9b9282fa68644d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-04 16:54:50 +0530

    modified

--- MYSQL DATABASES/CREATE DATABASE/CREATE DATABASE.sql
+++ MYSQL DATABASES/CREATE DATABASE/CREATE DATABASE.sql
@@ -7,5 +7,23 @@ mysql>
  */
  show databases ;
 /*
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| employees          |
+| information_schema |
+| mysql              |
+| performance_schema |
+| personal_database  |
+| personaldb         |
+| personnelschema    |
+| sakila             |
+| sys                |
+| testdatabase       |
+| world              |
++--------------------+
+11 rows in set (0.00 sec)
 
+mysql>
  */
