bc1f15e5c15aecd890626cd57e08ee44fa8f96a7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-04 18:03:27 +0530

    modified

--- MYSQL DATABASES/SELECT DATABASE/SELECT DATABASE.sql
+++ MYSQL DATABASES/SELECT DATABASE/SELECT DATABASE.sql
@@ -9,6 +9,11 @@ mysql>
 insert into personal_database.`customerid name`
 values(2333,'Arunkumar');
 /*
+mysql> insert into personal_database.`customerid name`
+    -> values(2333,'Arunkumar');
+Query OK, 1 row affected (0.01 sec)
 
+mysql>
  */
 use sakila;
+
