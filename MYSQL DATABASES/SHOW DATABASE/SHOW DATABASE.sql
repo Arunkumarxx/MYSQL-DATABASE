fbb2244457c62cdd619d9610bbec967f765ba781
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-04 18:10:37 +0530

    modified

--- MYSQL DATABASES/SHOW DATABASE/SHOW DATABASE.sql
+++ MYSQL DATABASES/SHOW DATABASE/SHOW DATABASE.sql
@@ -5,6 +5,22 @@ create database tempOne;
 show databases ;
 
 /*
-
++--------------------+
+| Database           |
++--------------------+
+| employees          |
+| information_schema |
+| mysql              |
+| performance_schema |
+| personal_database  |
+| personaldb         |
+| personnelschema    |
+| sakila             |
+| sys                |
+| tempone            |
+| testdatabase       |
+| world              |
++--------------------+
+12 rows in set (0.00 sec)
 
  */
