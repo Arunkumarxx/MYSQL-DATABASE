e72e8ea0e8b5de4cb8d5ad0875ad7ec6ff3f00e9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-04 18:03:17 +0530

    modified

--- MYSQL DATABASES/SELECT DATABASE/SELECT DATABASE.sql
+++ MYSQL DATABASES/SELECT DATABASE/SELECT DATABASE.sql
@@ -1,7 +1,14 @@
 #use databaseName;
 
 use personal_database;
+/*
+ mysql> use personal_database;
+Database changed
+mysql>
+ */
 insert into personal_database.`customerid name`
 values(2333,'Arunkumar');
-
+/*
+ 
+ */
 use sakila;
