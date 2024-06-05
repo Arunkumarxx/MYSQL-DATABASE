53109614ea208aacfc04ddda05a431d49814e1df
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-05 15:22:48 +0530

    modified

--- MYSQL USERS/CREATE USERS/CREATE USERS.sql
+++ MYSQL USERS/CREATE USERS/CREATE USERS.sql
@@ -5,3 +5,6 @@ CREATE USER 'sample'@'localhost' IDENTIFIED BY '123456';
 
 
 GRANT ALL PRIVILEGES ON database_name.* TO 'username'@'host';
+
+
+GRANT ALL PRIVILEGES ON * . * TO 'sample'@'localhost';
