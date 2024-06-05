b637f7fc566ac7eee578a1c32037a141ee3c5385
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-05 23:11:36 +0530

    modified

--- MYSQL TABLES/SHOW TABLES/SHOW TABLES.sql
+++ MYSQL TABLES/SHOW TABLES/SHOW TABLES.sql
@@ -5,3 +5,24 @@
  let see
  */
 show tables;
+/*
+ mysql> show tables;
++-----------------------------+
+| Tables_in_personal_database |
++-----------------------------+
+| categories                  |
+| customer                    |
+| customerid name             |
+| customers                   |
+| datatypes                   |
+| employees                   |
+| orderdetails                |
+| orders                      |
+| parent                      |
+| products                    |
+| shippers                    |
+| suppliers                   |
+| temp                        |
++-----------------------------+
+13 rows in set (0.00 sec)
+ */
