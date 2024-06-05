c0470a0c26d523fed1bfebcd5cce5f5e25b0fc93
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-05 17:30:36 +0530

    modified

--- MYSQL USERS/CHANGE PASSWORD/CHANGE PASSWORD.sql
+++ MYSQL USERS/CHANGE PASSWORD/CHANGE PASSWORD.sql
@@ -1,4 +1,14 @@
 select user from mysql.user;
 /*
-
++------------------+
+| user             |
++------------------+
+| Arunkumarxx      |
+| mysql.infoschema |
+| mysql.session    |
+| mysql.sys        |
+| root             |
++------------------+
  */
+
+ 
