6e49e2a95024149daa660797ebf9c930574e4e3a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-05 23:24:57 +0530

    modified

--- MYSQL TABLES/ALTER TABLES/ALTER TABLES.sql
+++ MYSQL TABLES/ALTER TABLES/ALTER TABLES.sql
@@ -2,7 +2,8 @@
  we use this alter clause for alter table which
  means for modify some values in table
  */
- alter table 
+ alter table customer
+     
 
 alter table customer
 modify id varchar(255);
