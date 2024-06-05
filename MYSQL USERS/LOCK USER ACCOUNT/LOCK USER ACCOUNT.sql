25054f2fe2fc0a32154fc34e21d789c04be014e4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-05 22:26:23 +0530

    modified

--- MYSQL USERS/LOCK USER ACCOUNT/LOCK USER ACCOUNT.sql
+++ MYSQL USERS/LOCK USER ACCOUNT/LOCK USER ACCOUNT.sql
@@ -7,3 +7,17 @@ Query OK, 0 rows affected (0.01 sec)
  */
 
 select user,host,account_locked  from mysql.user;
+/*
+ +------------------+-----------+----------------+
+| user             | host      | account_locked |
++------------------+-----------+----------------+
+| Arunkumarxx      | localhost | N              |
+| apex             | localhost | Y              |
+| mysql.infoschema | localhost | Y              |
+| mysql.session    | localhost | Y              |
+| mysql.sys        | localhost | Y              |
+| root             | localhost | N              |
++------------------+-----------+----------------+
+6 rows in set (0.00 sec)
+
+ */
