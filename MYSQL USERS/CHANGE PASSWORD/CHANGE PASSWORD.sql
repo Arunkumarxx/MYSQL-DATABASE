f2c13d0b218420ac75eba2e8b043f865e2a543c5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-05 20:55:26 +0530

    modified

--- MYSQL USERS/CHANGE PASSWORD/CHANGE PASSWORD.sql
+++ MYSQL USERS/CHANGE PASSWORD/CHANGE PASSWORD.sql
@@ -17,4 +17,13 @@ WHERE User = 'sample' AND Host = 'localhost';
 
 ALTER USER sample@localhost IDENTIFIED BY '000000';
 
+set password for 'Arunkumarxx'@'localhost'='arun';
+
+/*
+ personal_database> set password for 'Arunkumarxx'@'localhost'='some'
+[2024-06-05 20:51:53] completed in 8 ms
+personal_database> set password for 'Arunkumarxx'@'localhost'='some'
+[2024-06-05 20:52:08] completed in 7 ms
+ */
+
 SET PASSWORD FOR username@localhost = password_string;
