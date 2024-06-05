d27ac19406a27b92bca21dbbc3801ecac1720f91
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-05 21:46:44 +0530

    modified

--- MYSQL USERS/GRANT PRIVILEGES/GRANT PRIVILEGES.sql
+++ MYSQL USERS/GRANT PRIVILEGES/GRANT PRIVILEGES.sql
@@ -3,4 +3,4 @@ create user 'abcuser'@'localhost';
 use personal_database;
 grant create,update  on personal_database.customers to 'abcuser'@'localhost';
 
-set password for 'abcuser'@'localhost'
+set password for 'abcuser'@'localhost'='some';
