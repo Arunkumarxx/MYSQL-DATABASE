5e7d977fe599a89f514c4258c432c1e2672e669e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-05 22:31:34 +0530

    modified

--- MYSQL USERS/UNLOCK USER ACCOUNT/UNLOCK USER ACCOUNT.sql
+++ MYSQL USERS/UNLOCK USER ACCOUNT/UNLOCK USER ACCOUNT.sql
@@ -1,2 +1,14 @@
 select user,host,account_locked from mysql.user;
-
+/*
+ +------------------+-----------+----------------+
+| user             | host      | account_locked |
++------------------+-----------+----------------+
+| Arunkumarxx      | localhost | N              |
+| apex             | localhost | Y              |
+| mysql.infoschema | localhost | Y              |
+| mysql.session    | localhost | Y              |
+| mysql.sys        | localhost | Y              |
+| root             | localhost | N              |
++------------------+-----------+----------------+
+ */
+ 
