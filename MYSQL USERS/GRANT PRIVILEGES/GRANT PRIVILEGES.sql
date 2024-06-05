47ad1317107c4d29ca69eee07f080b8728224796
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-05 17:47:58 +0530

    modified

--- MYSQL USERS/GRANT PRIVILEGES/GRANT PRIVILEGES.sql
+++ MYSQL USERS/GRANT PRIVILEGES/GRANT PRIVILEGES.sql
@@ -4,4 +4,4 @@ GRANT privilege_type ON database_name.table_name TO 'Arunkumarxx'@'locahost';
 
 GRANT SELECT, INSERT, UPDATE ON personal_database.* TO 'my_user'@'localhost';
 
-select 
+select * from customers;
