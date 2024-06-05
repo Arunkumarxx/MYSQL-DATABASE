737c4d244ea480b53f8637b90b74669189a2f1ab
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-05 20:52:46 +0530

    modified

--- MYSQL USERS/GRANT PRIVILEGES/GRANT PRIVILEGES.sql
+++ MYSQL USERS/GRANT PRIVILEGES/GRANT PRIVILEGES.sql
@@ -1,5 +1,8 @@
 set password for 'Arunkumarxx'@'localhost'='some';
 
 /*
- 
+ personal_database> set password for 'Arunkumarxx'@'localhost'='some'
+[2024-06-05 20:51:53] completed in 8 ms
+personal_database> set password for 'Arunkumarxx'@'localhost'='some'
+[2024-06-05 20:52:08] completed in 7 ms
  */
