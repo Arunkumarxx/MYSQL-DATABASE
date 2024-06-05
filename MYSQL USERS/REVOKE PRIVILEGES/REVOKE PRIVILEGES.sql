d66972ecac0eced47a61b4737dd11f667246ba4b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-05 22:06:31 +0530

    modified

--- MYSQL USERS/REVOKE PRIVILEGES/REVOKE PRIVILEGES.sql
+++
@@ -1,7 +0,0 @@
-create user 'Arunku'@'localhost';
-
-show grants for 'Arunku'@'localhost';
-
-grant all on personal_database.customers to 'Arunku'@'localhost';
-
-show grants for 'Arunku'@'localhost';
