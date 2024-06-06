ab84e2e095b48ffc82f07e5b9d5e822586055ed0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-06 12:37:28 +0530

    modified

--- MYSQL TABLES/CLONE TABLES/CLONE TABLES.sql
+++ MYSQL TABLES/CLONE TABLES/CLONE TABLES.sql
@@ -2,5 +2,5 @@ use personal_database;
 
 create table detail as select * from customers;
 
-create table detail2  like detail
+create table detail2  like detail;
 insert into detail2  select * from customers;
