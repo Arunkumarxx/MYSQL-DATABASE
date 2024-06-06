f50ee5d192a1d22df300225c7074d9a6bdf43164
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-06 14:44:48 +0530

    modified

--- MYSQL TABLES/DESCRIBE TABLES/DESCRIBE TABLES.sql
+++ MYSQL TABLES/DESCRIBE TABLES/DESCRIBE TABLES.sql
@@ -28,3 +28,31 @@ desc customers;
 show columns  from customers;
 
 explain customers;
+
+/*
+
+mysql> describe cust;
++---------+---------------+------+-----+---------+----------------+
+| Field   | Type          | Null | Key | Default | Extra          |
++---------+---------------+------+-----+---------+----------------+
+| ID      | int           | NO   | PRI | NULL    | auto_increment |
+| NAME    | varchar(20)   | NO   |     | NULL    |                |
+| AGE     | int           | NO   |     | NULL    |                |
+| ADDRESS | char(25)      | YES  |     | NULL    |                |
+| SALARY  | decimal(18,2) | YES  |     | NULL    |                |
++---------+---------------+------+-----+---------+----------------+
+5 rows in set (0.00 sec)
+
+mysql> explain cust;
++---------+---------------+------+-----+---------+----------------+
+| Field   | Type          | Null | Key | Default | Extra          |
++---------+---------------+------+-----+---------+----------------+
+| ID      | int           | NO   | PRI | NULL    | auto_increment |
+| NAME    | varchar(20)   | NO   |     | NULL    |                |
+| AGE     | int           | NO   |     | NULL    |                |
+| ADDRESS | char(25)      | YES  |     | NULL    |                |
+| SALARY  | decimal(18,2) | YES  |     | NULL    |                |
++---------+---------------+------+-----+---------+----------------+
+5 rows in set (0.00 sec)
+
+ */
