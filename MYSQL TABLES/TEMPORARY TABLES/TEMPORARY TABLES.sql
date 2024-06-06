ae50de617b8d306e43379043aa100289d9090aee
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-06 13:54:21 +0530

    modified

--- MYSQL TABLES/TEMPORARY TABLES/TEMPORARY TABLES.sql
+++ MYSQL TABLES/TEMPORARY TABLES/TEMPORARY TABLES.sql
@@ -3,9 +3,9 @@ create procedure callss()
 begin
     create temporary  table tempxx as select customers.CustomerID, customers.CustomerName from
     customers;
-    select * from temp;
+    select * from ;
 end //delimiter ;
 
 call calls();
-drop table temp;
+
 show tables;
