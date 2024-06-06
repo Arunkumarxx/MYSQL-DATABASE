359d77a136b758c3dbbfbc38330ffb39a44d8851
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-06 18:31:56 +0530

    modified

--- MYSQL TABLES/TABLE LOCKING/TABLE LOCKING.sql
+++ MYSQL TABLES/TABLE LOCKING/TABLE LOCKING.sql
@@ -3,3 +3,10 @@ lock tables customers write;
 delete from customers
 where customerIDs>90;
 
+/*
+ personal_database> lock tables customers write
+[2024-06-06 18:29:43] completed in 1 ms
+personal_database> delete from customers
+                   where customerIDs>90
+[2024-06-06 18:29:45] 1 row affected in 4 ms
+ */
