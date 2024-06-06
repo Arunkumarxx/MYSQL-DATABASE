629893b3f92de12d2f4104ea0f69da15e6822cdf
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-06 12:50:47 +0530

    modified

--- MYSQL TABLES/TEMPORARY TABLES/TEMPORARY TABLES.sql
+++ MYSQL TABLES/TEMPORARY TABLES/TEMPORARY TABLES.sql
@@ -2,5 +2,5 @@ delimiter //
 create procedure call()
 begin
     create temporary  table temp as select customers.CustomerID, customers.CustomerName from
-                                                                                            
+    customers;
 end //delimiter ;
