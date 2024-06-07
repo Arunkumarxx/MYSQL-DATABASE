0e5aa77681e6e28ebe699f325736231d6a3a7f67
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-07 20:40:32 +0530

    modified

--- MYSQL OPERATORS AND CLAUSES/IS NOT NULL OPERATOR/IS NOT NULL OPERATOR.sql
+++ MYSQL OPERATORS AND CLAUSES/IS NOT NULL OPERATOR/IS NOT NULL OPERATOR.sql
@@ -2,6 +2,15 @@ select * from customers
 where CustomerName is not null limit 5;
 
 /*
-
++-------------+-------------------------+--------------------+-----------------+-------------+----------+---------+
+| customerIDs | CustomerName            | ContactName        | Address         | City        | pincode  | Country |
++-------------+-------------------------+--------------------+-----------------+-------------+----------+---------+
+|           1 | Alfreds Futterkiste     | Maria Anders       | Obere Str. 57   | Berlin      | 12209    | Germany |
+|           3 | Antonio Moreno Taquería | Antonio Moreno     | Mataderos 2312  | México D.F. | 05023    | Mexico  |
+|           4 | Around the Horn         | Thomas Hardy       | 120 Hanover Sq. | London      | WA1 1DP  | UK      |
+|           5 | Berglunds snabbköp      | Christina Berglund | Berguvsvägen 8  | Luleå       | S-958 22 | Sweden  |
+|           6 | Blauer See Delikatessen | Hanna Moos         | Forsterstr. 57  | Mannheim    | 68306    | Germany |
++-------------+-------------------------+--------------------+-----------------+-------------+----------+---------+
+5 rows in set (0.00 sec)
  */
 
