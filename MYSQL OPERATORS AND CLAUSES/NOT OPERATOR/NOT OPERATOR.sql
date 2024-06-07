ad283507ace846020d8ddd87af0f472f602c52a1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-07 20:32:34 +0530

    modified

--- MYSQL OPERATORS AND CLAUSES/NOT OPERATOR/NOT OPERATOR.sql
+++ MYSQL OPERATORS AND CLAUSES/NOT OPERATOR/NOT OPERATOR.sql
@@ -5,4 +5,6 @@ where CustomerName not like'a%';
 select * from customers
 where CustomerName not between  10 and 40;
 
-select * from cust
+select * from customers
+where Country not in ('france','Mexico');
+
