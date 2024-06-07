1b26e0c6d35748dd74fd34dd95d4d00b3f66a4d1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-07 20:17:30 +0530

    modified

--- MYSQL OPERATORS AND CLAUSES/HAVING CLAUSE/HAVING CLAUSE.sql
+++ MYSQL OPERATORS AND CLAUSES/HAVING CLAUSE/HAVING CLAUSE.sql
@@ -5,5 +5,15 @@ group by customerIDs  having customerIDs>23;
 select  name, sum(SALARY) from cust
 group by name having sum(SALARY)<4540 order by name;
 /*
-
+mysql> select  name, sum(SALARY) from cust
+    -> group by name having sum(SALARY)<4540 order by name;
++---------+-------------+
+| name    | sum(SALARY) |
++---------+-------------+
+| Kaushik |     2000.00 |
+| Khilan  |     1500.00 |
+| Komal   |     4500.00 |
+| Ramesh  |     2000.00 |
++---------+-------------+
+4 rows in set (0.00 sec)
  */
