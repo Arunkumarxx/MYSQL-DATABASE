e833bfd187903cff869e84579951632514f2b39a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-07 20:12:22 +0530

    modified

--- MYSQL OPERATORS AND CLAUSES/HAVING CLAUSE/HAVING CLAUSE.sql
+++ MYSQL OPERATORS AND CLAUSES/HAVING CLAUSE/HAVING CLAUSE.sql
@@ -2,4 +2,4 @@ select customerIDs,count(CustomerName) from customers
 group by customerIDs  having customerIDs>23;
 
 select ProductName, count(price) from products
-group by ProductName having count(price) <>1;
+group by count() having count(price) <>1;
