a7a7d0238006989d8d4eec68974e0f88d56f2ff3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-07 22:07:22 +0530

    modified

--- MYSQL OPERATORS AND CLAUSES/MINUS OPERATOR/MINUS OPERATOR.sql
+++ MYSQL OPERATORS AND CLAUSES/MINUS OPERATOR/MINUS OPERATOR.sql
@@ -3,6 +3,9 @@
 
 select * from customers
 where customerIDs not  in (select ProductID from products);
+/*
+ 
+ */
 
 select * from customers
-where not exists(select ProductID from products)
+where not exists(select ProductID from products);
