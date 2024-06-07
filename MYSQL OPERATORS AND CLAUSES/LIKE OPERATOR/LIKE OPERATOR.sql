e6890472d62f46c0006be415068934a76b6ce6e6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-07 20:07:42 +0530

    modified

--- MYSQL OPERATORS AND CLAUSES/LIKE OPERATOR/LIKE OPERATOR.sql
+++ MYSQL OPERATORS AND CLAUSES/LIKE OPERATOR/LIKE OPERATOR.sql
@@ -5,4 +5,5 @@ where CustomerName like 'a%';
 select ProductID from products
 where ProductName like '%a%';
 
-select customers.CustomerName from cust
+select customers.CustomerName from customers
+where CustomerName not like ''
