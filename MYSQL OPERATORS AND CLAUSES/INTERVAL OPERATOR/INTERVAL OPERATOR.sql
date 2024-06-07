f6b6e4d83107aecade95de739577311b33cfd521
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-07 22:44:02 +0530

    modified

--- MYSQL OPERATORS AND CLAUSES/INTERVAL OPERATOR/INTERVAL OPERATOR.sql
+++ MYSQL OPERATORS AND CLAUSES/INTERVAL OPERATOR/INTERVAL OPERATOR.sql
@@ -9,5 +9,11 @@ select '2024-03-23' + interval 10 day as Year;
  */
 select '2024-11-12' - interval 2 day + interval  3 hour as Result;
 /*
++---------------------+
+| Result              |
++---------------------+
+| 2024-11-10 03:00:00 |
++---------------------+
+1 row in set (0.00 sec)
 
  */
