7975bdfb184b4c7521eec6d7a9531a15d4c51efa
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-07 20:16:04 +0530

    modified

--- MYSQL OPERATORS AND CLAUSES/HAVING CLAUSE/HAVING CLAUSE.sql
+++ MYSQL OPERATORS AND CLAUSES/HAVING CLAUSE/HAVING CLAUSE.sql
@@ -2,5 +2,5 @@ select customerIDs,count(CustomerName) from customers
 group by customerIDs  having customerIDs>23;
 
 
-select  sum(SALARY) from cust
+select  name, sum(SALARY) from cust
 group by sum(SALARY) having sum(SALARY)<4540
