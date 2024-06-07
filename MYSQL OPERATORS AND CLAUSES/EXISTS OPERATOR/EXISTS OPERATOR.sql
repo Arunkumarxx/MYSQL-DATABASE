50648c90a6860431cf4943ccc98cf8034edc9628
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-07 20:30:42 +0530

    modified

--- MYSQL OPERATORS AND CLAUSES/EXISTS OPERATOR/EXISTS OPERATOR.sql
+++ MYSQL OPERATORS AND CLAUSES/EXISTS OPERATOR/EXISTS OPERATOR.sql
@@ -2,6 +2,6 @@ select * from orders
 where exists(select * from customers where customerIDs >50);
 
 select * from orders
-where exists(select  * from customers where customerIDs >100);
+where not exists(select  * from customers where customerIDs >100);
 
 
