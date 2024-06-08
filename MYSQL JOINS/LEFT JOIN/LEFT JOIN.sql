40a95889b9c10d44c763172402818d0f6bd78b1c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-08 14:50:54 +0530

    modified

--- MYSQL JOINS/LEFT JOIN/LEFT JOIN.sql
+++ MYSQL JOINS/LEFT JOIN/LEFT JOIN.sql
@@ -3,5 +3,5 @@ use sakila;
 select *from actor;
 
 select actor.first_name,actor.last_name,film.title
-from actor left join film  on actor_id=film_id;
+from actor left join film  on actor_id=film_id limit 2;
 
