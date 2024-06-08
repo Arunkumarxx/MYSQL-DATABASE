032d883b15a70c656a5be5bddf511e2d289367c6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-08 20:59:50 +0530

    modified

--- MYSQL KEYS/UNIQUE KEY/UNIQUE KEY.sql
+++ MYSQL KEYS/UNIQUE KEY/UNIQUE KEY.sql
@@ -5,3 +5,10 @@ create table custom (
 
 insert into custom values('Arun',1);
 insert into custom values('Arun',1);
+/*
+ personal_database> insert into custom values('Arun',1)
+[2024-06-08 20:59:37] 1 row affected in 5 ms
+personal_database> insert into custom values('Arun',1)
+[2024-06-08 20:59:41] [23000][1062] Duplicate entry 'Arun' for key 'custom.name'
+
+ */
