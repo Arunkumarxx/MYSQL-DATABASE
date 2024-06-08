f7242e220edb2aa53c809976da59534fa35728b2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-08 20:07:27 +0530

    modified

--- MYSQL JOINS/UPDATE JOIN/UPDATE JOIN.sql
+++ MYSQL JOINS/UPDATE JOIN/UPDATE JOIN.sql
@@ -13,6 +13,6 @@ INSERT INTO CUSTOMERS VALUES
 (2, 'Khilan', 25, 'Delhi', 1500.00 ),
 (3, 'Kaushik', 23, 'Kota', 2000.00 ),
 (4, 'Chaitali', 25, 'Mumbai', 6500.00 ),
-(5, 'Hardik', 27, 'Bhopal', 8500.00 ),
+(5, 'Hadik', 27, 'Bhopal', 8500.00 ),
 (6, 'Komal', 22, 'Hyderabad', 4500.00 ),
 (7, 'Muffy', 24, 'Indore', 10000.00 );
