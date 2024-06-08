f1f65a9821044a5f2c2210f270ae39f978022a05
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-08 14:26:51 +0530

    modified

--- MYSQL JOINS/INNER JOIN/INNER JOIN.sql
+++ MYSQL JOINS/INNER JOIN/INNER JOIN.sql
@@ -7,7 +7,7 @@ actor_id =customers.CustomerID;
 
 replace into customers
 (customerid, customername, contactname, address, city, postalcode, country)
-values(2,'Arunkumar','Arun','1/22 xyz','udumalpet',642201,'india')
+values(1,'Arunkumar','Arun','1/22 xyz','udumalpet',642201,'india')
 ;
 truncate customers;
 select * from customers;
