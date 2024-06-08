842359d70b1a5f4be38efb19ced4497eb6b160a3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-08 14:58:07 +0530

    modified

--- MYSQL JOINS/LEFT JOIN/LEFT JOIN.sql
+++ MYSQL JOINS/LEFT JOIN/LEFT JOIN.sql
@@ -3,8 +3,8 @@ use sakila;
 select *from actor;
 
 update actor
-set actor_id=5000
-where actor_id=5;
+set actor_id=5
+where actor_id=5000;
 
 select actor.first_name,actor.last_name,film.title
 from actor left join film  on actor_id=film_id;
