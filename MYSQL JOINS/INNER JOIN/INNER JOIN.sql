9f04a0fe3e85e87fd464d2309c11e5f6910686a7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-08 14:18:10 +0530

    modified

--- MYSQL JOINS/INNER JOIN/INNER JOIN.sql
+++ MYSQL JOINS/INNER JOIN/INNER JOIN.sql
@@ -2,4 +2,5 @@ use sakila;
 
 select rental.customer_id ,
 customers.CustomerName
-from rental,customers left join customers on CustomerID=customers.CustomerID;
+from rental,customers left join
+customers on CustomerID=customers.CustomerID;
