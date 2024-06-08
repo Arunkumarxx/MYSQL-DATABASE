a38e87ac3112af73f8b645f62dbcf66281e569a4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-08 14:14:09 +0530

    modified

--- MYSQL JOINS/USING JOINS/USING JOINS.sql
+++ MYSQL JOINS/USING JOINS/USING JOINS.sql
@@ -1,4 +1,4 @@
 use sakila;
-# we use joins for multiple purpose like check 
+# we use joins for multiple purpose like 
 select distinct rental.customer_id, staff.first_name
 from rental,staff where  staff.staff_id=customer_id;
