28efda55f88db59c6fdb410c71058dc8e8492b64
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-08 20:17:41 +0530

    modified

--- MYSQL JOINS/UNION VS JOIN/UNION VS JOIN.sql
+++ MYSQL JOINS/UNION VS JOIN/UNION VS JOIN.sql
@@ -20,5 +20,20 @@ select orders.CUSTOMER_ID from orders
 union all
 select id from customers;
 /*
-
++-------------+
+| CUSTOMER_ID |
++-------------+
+|           3 |
+|           3 |
+|           2 |
+|           4 |
+|           1 |
+|           2 |
+|           3 |
+|           4 |
+|           5 |
+|           6 |
+|           7 |
++-------------+
+11 rows in set (0.00 sec)
  */
