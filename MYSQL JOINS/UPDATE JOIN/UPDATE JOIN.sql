6d65a4b6c94a0ac250feb88ae73ff00814ed4ce3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-08 20:15:01 +0530

    modified

--- MYSQL JOINS/UPDATE JOIN/UPDATE JOIN.sql
+++ MYSQL JOINS/UPDATE JOIN/UPDATE JOIN.sql
@@ -5,5 +5,10 @@ set SALARY=SALARY+1000
 where customers.SALARY=2000;
 
 /*
-
+personal_database> update customers
+                   left join orders
+                   on CUSTOMER_ID=orders.CUSTOMER_ID
+                   set SALARY=SALARY+1000
+                   where customers.SALARY=2000
+[2024-06-08 20:13:31] 2 rows affected in 3 ms
  */
