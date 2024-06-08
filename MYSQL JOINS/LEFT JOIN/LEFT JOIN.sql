15154036b651b32b94d7aaa16278a88f217c4edc
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-08 15:01:47 +0530

    modified

--- MYSQL JOINS/LEFT JOIN/LEFT JOIN.sql
+++ MYSQL JOINS/LEFT JOIN/LEFT JOIN.sql
@@ -1,12 +1,4 @@
 use sakila;
 
-select *from actor;
-
-alter table actor
-drop primary key ;
-
-select actor.first_name,actor.last_name,film.title
-from actor left join film  on actor_id=film_id;
-
 
 
