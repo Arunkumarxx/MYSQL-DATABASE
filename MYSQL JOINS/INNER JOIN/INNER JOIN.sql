773e79d68863543923ff391c7a6c1256e784acae
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-08 14:25:50 +0530

    modified

--- MYSQL JOINS/INNER JOIN/INNER JOIN.sql
+++ MYSQL JOINS/INNER JOIN/INNER JOIN.sql
@@ -7,5 +7,5 @@ actor_id =customers.CustomerID;
 
 insert into customers
 (customerid, customername, contactname, address, city, postalcode, country)
-values(1,'Arunkumar','Arun','1/22 xyz','udumalpet',642201,)
+values(1,'Arunkumar','Arun','1/22 xyz','udumalpet',642201,'india')
 ;
