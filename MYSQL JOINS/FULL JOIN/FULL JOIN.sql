4ee41cdcce007965eda75e0c744d41cac6cf2934
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-08 17:02:50 +0530

    modified

--- MYSQL JOINS/FULL JOIN/FULL JOIN.sql
+++ MYSQL JOINS/FULL JOIN/FULL JOIN.sql
@@ -2,5 +2,17 @@ select name,amount
 from customers join orders on CUSTOMER_ID=id;
 
 /*
-
++----------+---------+
+| name     | amount  |
++----------+---------+
+| Kaushik  | 3000.00 |
+| Kaushik  | 1500.00 |
+| Khilan   | 1560.00 |
+| Chaitali | 2060.00 |
+| Kaushik  | 3000.00 |
+| Kaushik  | 1500.00 |
+| Khilan   | 1560.00 |
+| Chaitali | 2060.00 |
++----------+---------+
+8 rows in set (0.00 sec)
  */
