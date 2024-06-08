53411baf46ab99b3b6ec5d8129a52924386dc64a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-08 15:05:00 +0530

    modified

--- MYSQL JOINS/LEFT JOIN/LEFT JOIN.sql
+++ MYSQL JOINS/LEFT JOIN/LEFT JOIN.sql
@@ -1,3 +1,10 @@
 use personal_database;
 
-create table 
+CREATE TABLE CUSTOMERS (
+   ID INT NOT NULL,
+   NAME VARCHAR (20) NOT NULL,
+   AGE INT NOT NULL,
+   ADDRESS CHAR (25),
+   SALARY DECIMAL (18, 2),
+   PRIMARY KEY (ID)
+);
