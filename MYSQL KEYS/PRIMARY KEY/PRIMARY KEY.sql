a471426a72e26044ba974ab256659828d89fafab
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-09 16:18:46 +0530

    modified

--- MYSQL KEYS/PRIMARY KEY/PRIMARY KEY.sql
+++ MYSQL KEYS/PRIMARY KEY/PRIMARY KEY.sql
@@ -2,5 +2,5 @@
      id int primary key ,
      name varchar(255)
  );
-insert into stduent values(1,'Arun');
+insert into stduent values(1,'Arun');#done 
 insert into stduent values(1,'xyz');#[2024-06-09 16:18:28] [23000][1062] Duplicate entry '1' for key 'stduent.PRIMARY'
