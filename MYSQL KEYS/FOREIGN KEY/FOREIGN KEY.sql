382e5b17dd5d9069e87c5b900e6f9b0072c52062
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-09 16:31:24 +0530

    modified

--- MYSQL KEYS/FOREIGN KEY/FOREIGN KEY.sql
+++ MYSQL KEYS/FOREIGN KEY/FOREIGN KEY.sql
@@ -8,7 +8,7 @@ create table studentID(
 create table classA(
     id int primary key ,
     name varchar(255) ,
-    constraint  foreign key (id) references studentID(id)
+  constraint somes   foreign key (id) references studentID(id)
 );
 insert into studentID values(1,'Arun'),(2,'xyz');
 insert into classA values(1,'abc'),(2,'qwe'),(3,'zxa');
