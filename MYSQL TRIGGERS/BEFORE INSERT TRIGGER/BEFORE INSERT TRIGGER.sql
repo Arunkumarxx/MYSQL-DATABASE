fa115b33b9f2c588d9a16553ad071be3855503c9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-10 13:50:59 +0530

    modified

--- MYSQL TRIGGERS/BEFORE INSERT TRIGGER/BEFORE INSERT TRIGGER.sql
+++ MYSQL TRIGGERS/BEFORE INSERT TRIGGER/BEFORE INSERT TRIGGER.sql
@@ -2,7 +2,8 @@
  create trigger isFailOrPass before insert on student
      begin
          if new.score<35 then set new.grade='FAIL';
-         else set new.grade='PASS'
+         else set new.grade='PASS';
+         end if;
      end //
 
      // delimiter ;
