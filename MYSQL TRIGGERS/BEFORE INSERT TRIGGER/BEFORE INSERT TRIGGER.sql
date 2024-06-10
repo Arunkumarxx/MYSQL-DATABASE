6a9a26939d3070d7e498c7978cce5f23fc021b21
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-10 13:52:20 +0530

    modified

--- MYSQL TRIGGERS/BEFORE INSERT TRIGGER/BEFORE INSERT TRIGGER.sql
+++ MYSQL TRIGGERS/BEFORE INSERT TRIGGER/BEFORE INSERT TRIGGER.sql
@@ -1,7 +1,7 @@
 alter table student
 add column grade varchar(255);
 delimiter //
- create trigger isFailOrPass before insert on student
+ create trigger isFailOrPass before insert on student for each row
      begin
          if new.score<35 then set new.grade='FAIL';
          else set new.grade='PASS';
