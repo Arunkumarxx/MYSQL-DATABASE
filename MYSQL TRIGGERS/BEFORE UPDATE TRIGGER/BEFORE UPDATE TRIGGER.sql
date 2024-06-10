75cb3d9938678f262099e1e2574df80ad77b9e3e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-10 20:32:32 +0530

    modified

--- MYSQL TRIGGERS/BEFORE UPDATE TRIGGER/BEFORE UPDATE TRIGGER.sql
+++ MYSQL TRIGGERS/BEFORE UPDATE TRIGGER/BEFORE UPDATE TRIGGER.sql
@@ -11,6 +11,5 @@ delimiter //
 create trigger CheckAgeIsNotNeg before update on personInfo
     for each row
     begin
-        update personInfo 
-            if new.age<0 then set age='Aghe cannot be negative'
+        if new.age<0 
     end //
