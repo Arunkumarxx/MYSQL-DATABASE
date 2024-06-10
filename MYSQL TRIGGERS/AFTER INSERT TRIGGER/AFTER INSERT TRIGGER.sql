1d79ea6c2dffaa12621d74a7aa93c543538625ee
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-10 20:03:12 +0530

    modified

--- MYSQL TRIGGERS/AFTER INSERT TRIGGER/AFTER INSERT TRIGGER.sql
+++ MYSQL TRIGGERS/AFTER INSERT TRIGGER/AFTER INSERT TRIGGER.sql
@@ -1,10 +1,11 @@
 create table remainder(
      id int ,
     birthDATE date,
-    ,
+     name varchar(255),
     notifications varchar(255)
 );
 
+drop table remainder;
 delimiter //
 create trigger Wish after insert on remainder for each row
     begin
