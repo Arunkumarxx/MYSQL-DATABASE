8cd0f52d3a9159eb0165ca68720c1ef68c996ec3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-10 20:51:00 +0530

    modified

--- MYSQL TRIGGERS/AFTER UPDATE TRIGGER/AFTER UPDATE TRIGGER.sql
+++ MYSQL TRIGGERS/AFTER UPDATE TRIGGER/AFTER UPDATE TRIGGER.sql
@@ -1,6 +1,6 @@
 insert into personInfo values (2,'john wick',19,'2004-08-21');
 DELIMITER //
-CREATE TRIGGER before_update_trigger
+CREATE TRIGGER after_update_trigger
 after UPDATE ON personInfo FOR EACH ROW
 BEGIN
    IF age< 0
