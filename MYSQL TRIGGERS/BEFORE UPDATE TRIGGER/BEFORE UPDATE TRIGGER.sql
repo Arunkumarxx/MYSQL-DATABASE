6239dda9b3bff3fbd50baa29ef9670628f152964
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-10 20:44:29 +0530

    modified

--- MYSQL TRIGGERS/BEFORE UPDATE TRIGGER/BEFORE UPDATE TRIGGER.sql
+++ MYSQL TRIGGERS/BEFORE UPDATE TRIGGER/BEFORE UPDATE TRIGGER.sql
@@ -11,12 +11,12 @@ DELIMITER //
 CREATE TRIGGER before_update_trigger
 BEFORE UPDATE ON personInfo FOR EACH ROW
 BEGIN
-   IF NEW.AGE < 0
+   IF age< 0
    THEN SIGNAL SQLSTATE '45000'
    SET MESSAGE_TEXT = 'Age Cannot be Negative';
 END IF;
 END //
 DELIMITER ;
 insert into personInfo values(2,'szkx',-1,'2004-09-22');
-
+drop trigger 
 select * from personInfo;
