7f4324f5db97ad8ca9f695854f86e50e498363d6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-10 20:13:05 +0530

    modified

--- MYSQL TRIGGERS/AFTER INSERT TRIGGER/AFTER INSERT TRIGGER.sql
+++ MYSQL TRIGGERS/AFTER INSERT TRIGGER/AFTER INSERT TRIGGER.sql
@@ -21,5 +21,10 @@ insert into remainder values(1,'2004-08-21','Arun kumar');
 
 select * from birthDayList;
 /*
-
++------+------------+---------------------------+
+| id   | birthDate  | notifications             |
++------+------------+---------------------------+
+|    1 | 2004-08-21 | Happy BirthDay,Arun kumar |
++------+------------+---------------------------+
+1 row in set (0.00 sec)
  */
