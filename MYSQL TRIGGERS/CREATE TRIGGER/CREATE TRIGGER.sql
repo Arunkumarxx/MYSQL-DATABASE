8377bbfc40bf6d47d659c61ac00ba25ab6320ca0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-10 13:19:57 +0530

    modified

--- MYSQL TRIGGERS/CREATE TRIGGER/CREATE TRIGGER.sql
+++ MYSQL TRIGGERS/CREATE TRIGGER/CREATE TRIGGER.sql
@@ -8,6 +8,6 @@ delimiter //
 create trigger IsScoreLessThanOrEqualToZero
     before insert on student for each row
     begin
-        if score<=1 then 
+        if score<=1 then score=23
     end
 //delimiter ;
