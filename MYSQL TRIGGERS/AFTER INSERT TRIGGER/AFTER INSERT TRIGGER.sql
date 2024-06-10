d2880cb90fc1b3cfbdf0f23772e841fe0fff24e6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-10 20:06:38 +0530

    modified

--- MYSQL TRIGGERS/AFTER INSERT TRIGGER/AFTER INSERT TRIGGER.sql
+++ MYSQL TRIGGERS/AFTER INSERT TRIGGER/AFTER INSERT TRIGGER.sql
@@ -13,7 +13,7 @@ delimiter //
 create trigger Wish after insert on remainder for each row
     begin
         if new.birthDATE is not null and new.name is not null
-            then insert into 
+            then insert into birthdaylist()
             end if;
     end //
  // delimiter ;
