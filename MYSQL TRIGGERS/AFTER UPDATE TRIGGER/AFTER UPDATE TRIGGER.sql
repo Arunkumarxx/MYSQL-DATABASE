79f38d6893d1f47729d4dc12c6e6e5a51875ec6f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-10 20:50:49 +0530

    modified

--- MYSQL TRIGGERS/AFTER UPDATE TRIGGER/AFTER UPDATE TRIGGER.sql
+++ MYSQL TRIGGERS/AFTER UPDATE TRIGGER/AFTER UPDATE TRIGGER.sql
@@ -1,7 +1,7 @@
 insert into personInfo values (2,'john wick',19,'2004-08-21');
 DELIMITER //
 CREATE TRIGGER before_update_trigger
-BEFORE UPDATE ON personInfo FOR EACH ROW
+after UPDATE ON personInfo FOR EACH ROW
 BEGIN
    IF age< 0
    THEN SIGNAL SQLSTATE '45000'
