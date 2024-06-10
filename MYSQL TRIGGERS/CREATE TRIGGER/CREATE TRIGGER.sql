8db612b29f74edc78be5360c85e81583155dae0b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-10 13:22:06 +0530

    modified

--- MYSQL TRIGGERS/CREATE TRIGGER/CREATE TRIGGER.sql
+++ MYSQL TRIGGERS/CREATE TRIGGER/CREATE TRIGGER.sql
@@ -4,15 +4,11 @@ create table student
     age int,
     score int
 );
-DELIMITER $$
-
-CREATE TRIGGER IsScoreLessThanZero
-    BEFORE INSERT ON student
-    FOR EACH ROW
-BEGIN
-    IF NEW.score < 0 THEN
-        SET NEW.score = 0;
-    END IF;
-END $$
-
-DELIMITER ;
+delimiter //
+create trigger IsScoreLessThanZero
+    before insert on student for each row
+    begin
+        if student.score<0 then set student.score=0;
+            end if;
+    end
+//delimiter ;
