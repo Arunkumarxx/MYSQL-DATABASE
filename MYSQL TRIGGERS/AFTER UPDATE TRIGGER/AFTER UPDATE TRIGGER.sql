cc3bad2975b5ede40885328a79844b1058b754c3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-10 20:50:17 +0530

    modified

---
+++ MYSQL TRIGGERS/AFTER UPDATE TRIGGER/AFTER UPDATE TRIGGER.sql
@@ -0,0 +1,12 @@
+insert into personInfo values (1,'Arunkumar',19,'2004-08-21');
+
+DELIMITER //
+CREATE TRIGGER before_update_trigger
+BEFORE UPDATE ON personInfo FOR EACH ROW
+BEGIN
+   IF age< 0
+   THEN SIGNAL SQLSTATE '45000'
+   SET MESSAGE_TEXT = 'Age Cannot be Negative';
+END IF;
+END //
+DELIMITER ;
