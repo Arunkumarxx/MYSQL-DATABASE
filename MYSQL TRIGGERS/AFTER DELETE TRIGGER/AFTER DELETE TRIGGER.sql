ea6bf07afa4d3657e080350a18f8d896b29c06d2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-12 21:35:37 +0530

    modified

--- MYSQL TRIGGERS/AFTER DELETE TRIGGER/AFTER DELETE TRIGGER.sql
+++ MYSQL TRIGGERS/AFTER DELETE TRIGGER/AFTER DELETE TRIGGER.sql
@@ -29,6 +29,7 @@ delimiter //
 create trigger AfterDelete after delete on customers
     for each row
     begin
-        insert into old_customers value (old.ID,OLD.ID,OLD.age,old.ADDRESS,);
+        insert into old_customers value (old.ID,OLD.ID,OLD.age,old.ADDRESS,old.SALARY);
+        
     end //
     delimiter ;
