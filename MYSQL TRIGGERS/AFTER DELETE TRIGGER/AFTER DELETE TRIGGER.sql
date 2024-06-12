413b64f60a1845547b58033b2aacd45e0da2eac0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-12 20:48:49 +0530

    modified

--- MYSQL TRIGGERS/AFTER DELETE TRIGGER/AFTER DELETE TRIGGER.sql
+++ MYSQL TRIGGERS/AFTER DELETE TRIGGER/AFTER DELETE TRIGGER.sql
@@ -6,3 +6,12 @@ CREATE TABLE CUSTOMERS(
    SALARY DECIMAL(18, 2),
    PRIMARY KEY(ID)
 );
+
+INSERT INTO CUSTOMERS VALUES
+(1, 'Ramesh', 32, 'Ahmedabad', 2000.00 ),
+(2, 'Khilan', 25, 'Delhi', 1500.00 ),
+(3, 'Kaushik', 23, 'Kota', 2000.00 ),
+(4, 'Chaitali', 25, 'Mumbai', 6500.00 ),
+(5, 'Hardik', 27, 'Bhopal', 8500.00 ),
+(6, 'Komal', 22, 'MP', 4500.00 ),
+(7, 'Muffy', 24, 'Indore', 10000.00 );
