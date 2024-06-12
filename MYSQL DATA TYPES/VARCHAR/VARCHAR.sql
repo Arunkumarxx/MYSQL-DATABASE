e362353d9f7538726101100dfb254403f166512b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-12 22:26:35 +0530

    modified

--- MYSQL DATA TYPES/VARCHAR/VARCHAR.sql
+++ MYSQL DATA TYPES/VARCHAR/VARCHAR.sql
@@ -1,14 +1,1 @@
-create table DataTypes (
-    integers int,
-    tinyinteger tinyint,
-    smallInteger smallint,
-    meidumInt mediumint,
-    bigInteger bigint,
-    floats float,
-    doubles double,
-    decimals decimal(4,2)
-);
 
-insert into datatypes values (100233,122,2333,232323,3294392,23.233,2324.2324,44);
-
-select * from datatypes;
