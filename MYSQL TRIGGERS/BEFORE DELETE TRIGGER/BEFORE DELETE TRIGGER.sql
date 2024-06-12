fd15c5d46ac8fc53e134f79dade4538debdac676
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-12 17:29:29 +0530

    modified

--- MYSQL TRIGGERS/BEFORE DELETE TRIGGER/BEFORE DELETE TRIGGER.sql
+++ MYSQL TRIGGERS/BEFORE DELETE TRIGGER/BEFORE DELETE TRIGGER.sql
@@ -12,6 +12,6 @@ create table student(
     grade int
 );
 
-insert into student values( );
+insert into student values('Arunkumar',);
 delimiter //
 create trigger beforeDelete before delete on student for each row
