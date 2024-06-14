a392a58ce34dc252f813480b747fe4de0c1ed7be
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-14 22:33:51 +0530

    modified

--- MYSQL REGULAR EXPRESSIONS/REGULAR EXPRESSIONS/REGULAR EXPRESSIONS.sql
+++ MYSQL REGULAR EXPRESSIONS/REGULAR EXPRESSIONS/REGULAR EXPRESSIONS.sql
@@ -152,6 +152,6 @@ create table NameList(
 
 insert into namelist values('abedscs');
 
-insert into personal_database.namelist
+insert into personal_database.namelist values();
 
 select * from namelist where name regexp 'a.*s$';
