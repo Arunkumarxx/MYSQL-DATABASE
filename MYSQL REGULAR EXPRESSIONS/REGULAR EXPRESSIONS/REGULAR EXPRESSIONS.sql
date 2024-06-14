ae35d044d7d25e2723796dade307f413d7143523
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-14 22:02:04 +0530

    modified

--- MYSQL REGULAR EXPRESSIONS/REGULAR EXPRESSIONS/REGULAR EXPRESSIONS.sql
+++ MYSQL REGULAR EXPRESSIONS/REGULAR EXPRESSIONS/REGULAR EXPRESSIONS.sql
@@ -76,4 +76,4 @@ select * from customers where name regexp '[aeiou]';
 2 rows in set (0.00 sec)
  */
 
- select * from customers where name regexp 'ad*';
+ select * from customers where name regexp 'z';
