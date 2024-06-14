e9f562eaaf22f74a35180d8cfe28f4701a3423cc
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-14 21:49:08 +0530

    modified

--- MYSQL REGULAR EXPRESSIONS/REGULAR EXPRESSIONS/REGULAR EXPRESSIONS.sql
+++ MYSQL REGULAR EXPRESSIONS/REGULAR EXPRESSIONS/REGULAR EXPRESSIONS.sql
@@ -9,6 +9,11 @@ create table customers
 
 insert into customers (name,age,address,salary)
 values
-(''),
-
+(1, 'Ramesh', 32, 'Ahmedabad', 2000.00 ),
+(2, 'Khilan', 25, 'Delhi', 1500.00 ),
+(3, 'Kaushik', 23, 'Kota', 2000.00 ),
+(4, 'Chaitali', 25, 'Mumbai', 6500.00 ),
+(5, 'Hardik', 27, 'Bhopal', 8500.00 ),
+(6, 'Komal', 22, 'Hyderabad', 4500.00 ),
+(7, 'Muffy', 24, 'Indore', 10000.00 );
 ;
