83ea05c7fb333191f654e567c3e695d4c14fa9b0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-14 22:10:58 +0530

    modified

--- MYSQL REGULAR EXPRESSIONS/REGULAR EXPRESSIONS/REGULAR EXPRESSIONS.sql
+++ MYSQL REGULAR EXPRESSIONS/REGULAR EXPRESSIONS/REGULAR EXPRESSIONS.sql
@@ -92,4 +92,4 @@ select * from customers where name regexp '[aeiou]';
 7 rows in set (0.00 sec)
  */
 
-select * from customers where  name not  regexp  '^[^a]';
+select * from customers where  name not  regexp  '[abc]';
