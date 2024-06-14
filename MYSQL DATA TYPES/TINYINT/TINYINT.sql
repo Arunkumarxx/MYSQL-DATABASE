fcadc4c9801d461246e6bc914991ca5a73f5c6e3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-14 13:26:21 +0530

    modified

--- MYSQL DATA TYPES/TINYINT/TINYINT.sql
+++ MYSQL DATA TYPES/TINYINT/TINYINT.sql
@@ -6,3 +6,17 @@ insert into rangelowOrer values (-124)
 ,(-128),(120);
 
 select * from rangelowOrer;
+
+/*
+ +----------+
+| tinyInts |
++----------+
+|     -124 |
+|     -124 |
+|     -128 |
+|     -124 |
+|     -128 |
+|      120 |
++----------+
+6 rows in set (0.00 sec)
+ */
