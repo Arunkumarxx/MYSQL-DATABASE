e90e21b583e4bd406f74bde1a7a0f5690622c86f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-14 21:55:48 +0530

    modified

--- MYSQL REGULAR EXPRESSIONS/REGULAR EXPRESSIONS/REGULAR EXPRESSIONS.sql
+++ MYSQL REGULAR EXPRESSIONS/REGULAR EXPRESSIONS/REGULAR EXPRESSIONS.sql
@@ -21,5 +21,15 @@ select * from customers;
 
 select * from customers where name regexp 'a';
 /*
-
++----+----------+------+-----------+---------+
+| id | name     | age  | address   | salary  |
++----+----------+------+-----------+---------+
+|  1 | Ramesh   |   32 | Ahmedabad | 2000.00 |
+|  2 | Khilan   |   25 | Delhi     | 1500.00 |
+|  3 | Kaushik  |   23 | Kota      | 2000.00 |
+|  4 | Chaitali |   25 | Mumbai    | 6500.00 |
+|  5 | Hardik   |   27 | Bhopal    | 8500.00 |
+|  6 | Komal    |   22 | Hyderabad | 4500.00 |
++----+----------+------+-----------+---------+
+6 rows in set (0.00 sec)
  */
