4dd2a2fb40012ce45f1f3ddbe0c7ca315d0d1b1c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-14 23:02:16 +0530

    modified

--- MYSQL REGULAR EXPRESSIONS/RLIKE OPERATOR/RLIKE OPERATOR.sql
+++ MYSQL REGULAR EXPRESSIONS/RLIKE OPERATOR/RLIKE OPERATOR.sql
@@ -10,5 +10,11 @@ select 'arunkumar' rlike'[acb]';
  */
 select 'some' rlike '[a-b]';
 /*
-
++----------------------+
+| 'some' rlike '[a-b]' |
++----------------------+
+|                    0 |
++----------------------+
+1 row in set (0.00 sec)
  */
+ 
