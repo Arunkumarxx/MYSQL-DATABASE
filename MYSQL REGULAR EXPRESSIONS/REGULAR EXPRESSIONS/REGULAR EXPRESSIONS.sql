c35537ade65475eeba5c9715daa47b7b8ee70f91
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-14 21:47:04 +0530

    modified

--- MYSQL REGULAR EXPRESSIONS/REGULAR EXPRESSIONS/REGULAR EXPRESSIONS.sql
+++ MYSQL REGULAR EXPRESSIONS/REGULAR EXPRESSIONS/REGULAR EXPRESSIONS.sql
@@ -1,9 +1,10 @@
 create table customers
 (
-    id int auto_increment ,
+    id int auto_increment primary key ,
     name varchar(16),
     age tinyint,
     address varchar(255),
     salary decimal(10,2)
 )
+
 ;
