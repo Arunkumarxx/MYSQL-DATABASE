2519130b45c0e440cdfe20e2faf849910050fb6a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-14 13:24:39 +0530

    modified

--- MYSQL DATA TYPES/BIT/BIT.sql
+++ MYSQL DATA TYPES/BIT/BIT.sql
@@ -6,3 +6,16 @@ insert into BinaryValueOfNumber
 values(23),(123),(0342),(0223),(-2333);
 
 select *  from binaryvalueofnumber;
+/*
+ +--------------------+
+| number             |
++--------------------+
+| 0x0000000000000017 |
+| 0x0000000000000017 |
+| 0x000000000000007B |
+| 0x0000000000000156 |
+| 0x00000000000000DF |
+| 0xFFFFFFFFFFFFF6E3 |
++--------------------+
+6 rows in set (0.00 sec)
+ */
