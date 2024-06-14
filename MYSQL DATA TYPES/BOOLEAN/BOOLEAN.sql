b2c1ae80f2e7c56b970d6d9a68f7b78bafe2b9da
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-14 12:21:06 +0530

    modified

--- MYSQL DATA TYPES/BOOLEAN/BOOLEAN.sql
+++ MYSQL DATA TYPES/BOOLEAN/BOOLEAN.sql
@@ -8,7 +8,7 @@ create table BikeDetails(
 delimiter //
 create trigger isBlack after insert on bikedetails for each row
     begin
-        if isBlack like '%k' then
+        if  like '%k' then
         insert into bikedetails(isBlack) values(true);
         else insert into bikedetails(isBlack) values(false);
         end if;
