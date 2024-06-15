b9b820b236f523b250c8b10c7fe0c4949699c960
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-15 12:48:05 +0530

    modified

--- MYSQL FUNCTIONS & OPERATORS/DATE AND TIME FUNCTIONS/DATE AND TIME FUNCTIONS.sql
+++ MYSQL FUNCTIONS & OPERATORS/DATE AND TIME FUNCTIONS/DATE AND TIME FUNCTIONS.sql
@@ -18,7 +18,7 @@ select adddate('2004-08-21','23');
 1 row in set (0.00 sec)
  */
 
- select curdate()+;
+ select curdate();
 
 /*
 +------------+
@@ -27,3 +27,5 @@ select adddate('2004-08-21','23');
 | 2024-06-15 |
 +------------+
  */
+
+ 
