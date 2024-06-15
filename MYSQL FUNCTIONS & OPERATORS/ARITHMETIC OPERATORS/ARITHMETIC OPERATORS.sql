59c2adf502112fd77e22a394e96d45079014922d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-15 13:06:42 +0530

    modified

--- MYSQL FUNCTIONS & OPERATORS/ARITHMETIC OPERATORS/ARITHMETIC OPERATORS.sql
+++ MYSQL FUNCTIONS & OPERATORS/ARITHMETIC OPERATORS/ARITHMETIC OPERATORS.sql
@@ -9,3 +9,59 @@ select 10-5;
 select -10;
 
 select 123/2;
+
+/*
+ mysql> select 10%23;
++-------+
+| 10%23 |
++-------+
+|    10 |
++-------+
+1 row in set (0.00 sec)
+
+mysql>
+mysql> select 10*2;
++------+
+| 10*2 |
++------+
+|   20 |
++------+
+1 row in set (0.00 sec)
+
+mysql>
+mysql> select 10+100;
++--------+
+| 10+100 |
++--------+
+|    110 |
++--------+
+1 row in set (0.00 sec)
+
+mysql>
+mysql> select 10-5;
++------+
+| 10-5 |
++------+
+|    5 |
++------+
+1 row in set (0.00 sec)
+
+mysql>
+mysql> select -10;
++-----+
+| -10 |
++-----+
+| -10 |
++-----+
+1 row in set (0.00 sec)
+
+mysql>
+mysql> select 123/2;
++---------+
+| 123/2   |
++---------+
+| 61.5000 |
++---------+
+1 row in set (0.00 sec)
+
+ */
