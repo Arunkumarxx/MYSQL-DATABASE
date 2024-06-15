3835fe777a40430183f19fc6a39bdb2c7fa4f561
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-15 11:59:27 +0530

    modified

--- MYSQL REGULAR EXPRESSIONS/REGEXP_REPLACE() FUNCTION/REGEXP_REPLACE() FUNCTION.sql
+++ MYSQL REGULAR EXPRESSIONS/REGEXP_REPLACE() FUNCTION/REGEXP_REPLACE() FUNCTION.sql
@@ -1,19 +1,3 @@
-update customers
-set name='Arun kumar'
-where regexp_replace(name,'^a','i');
-
-select * from customers;
-/*
-+----+---------+------+
-| id | name    | age  |
-+----+---------+------+
-|  1 | Alice   |   25 |
-|  2 | Bob     |   32 |
-|  3 | Charlie |   29 |
-|  4 | David   |   37 |
-|  5 | Eve     |   41 |
-|  6 | Oscar   |   55 |
-|  7 | M@rk!   |   33 |
-|  8 | Hexa1F2 |   28 |
-+----+---------+------+
- */
+UPDATE customers
+SET name = 'Arun Kumar'
+WHERE REGEXP_LIKE(name, '^a', 'i');
