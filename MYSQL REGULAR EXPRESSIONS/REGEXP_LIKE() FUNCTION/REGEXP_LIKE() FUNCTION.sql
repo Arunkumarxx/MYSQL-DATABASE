d401bb265632fd0893854d91e623268fbcfea8f0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-15 11:15:17 +0530

    modified

---
+++ MYSQL REGULAR EXPRESSIONS/REGEXP_LIKE() FUNCTION/REGEXP_LIKE() FUNCTION.sql
@@ -0,0 +1,15 @@
+CREATE TABLE customers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT
+);
+
+INSERT INTO customers (name, age) VALUES
+('Alice', 25),
+('Bob', 32),
+('Charlie', 29),
+('David', 37),
+('Eve', 41),
+('Oscar', 55),
+('M@rk!', 33),
+('Hexa1F2', 28);
