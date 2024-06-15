ca7d661c41aedc15c9c15ace0c2282564c022be7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-15 11:59:12 +0530

    modified

--- MYSQL REGULAR EXPRESSIONS/REGEXP_REPLACE() FUNCTION/REGEXP_REPLACE() FUNCTION.sql
+++ MYSQL REGULAR EXPRESSIONS/REGEXP_REPLACE() FUNCTION/REGEXP_REPLACE() FUNCTION.sql
@@ -4,5 +4,16 @@ where regexp_replace(name,'^a','i');
 
 select * from customers;
 /*
-
++----+---------+------+
+| id | name    | age  |
++----+---------+------+
+|  1 | Alice   |   25 |
+|  2 | Bob     |   32 |
+|  3 | Charlie |   29 |
+|  4 | David   |   37 |
+|  5 | Eve     |   41 |
+|  6 | Oscar   |   55 |
+|  7 | M@rk!   |   33 |
+|  8 | Hexa1F2 |   28 |
++----+---------+------+
  */
