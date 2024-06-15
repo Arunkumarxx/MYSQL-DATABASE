18e62c6b484f47cc8985612c89b39ea0ead913d9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-15 10:47:05 +0530

    modified

--- MYSQL REGULAR EXPRESSIONS/MYSQL -REGEXP_INSTR() FUNCTION/MYSQL -REGEXP_INSTR() FUNCTION.sql
+++ MYSQL REGULAR EXPRESSIONS/MYSQL -REGEXP_INSTR() FUNCTION/MYSQL -REGEXP_INSTR() FUNCTION.sql
@@ -4,5 +4,18 @@ SELECT name, REGEXP_INSTR(name, 'a{1}') AS Position
 FROM customers;
 
 /*
-
++----------+----------+
+| name     | Position |
++----------+----------+
+| Ramesh   |        2 |
+| Khilan   |        5 |
+| Kaushik  |        2 |
+| Chaitali |        3 |
+| Hardik   |        2 |
+| Komal    |        4 |
+| Muffy    |        0 |
++----------+----------+
+7 rows in set (0.00 sec)
  */
+
+ 
