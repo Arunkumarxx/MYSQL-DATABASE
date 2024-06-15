63ce910ad1808fd30fd2fb665cc53173ed605a88
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-15 14:29:28 +0530

    modified

--- MYSQL FUNCTIONS & OPERATORS/STRING FUNCTIONS/STRING FUNCTIONS.sql
+++ MYSQL FUNCTIONS & OPERATORS/STRING FUNCTIONS/STRING FUNCTIONS.sql
@@ -21,4 +21,6 @@ select field('fd','some','creaet','fd','fdas');
 
 SELECT INSERT('Tutorialspoint', 20, 5, 'Tutorials');
 
-select insert('kumar',1,3,'Arun')
+select insert('kumar',1,3,'Arun');
+
+select instr('kumar','a');
