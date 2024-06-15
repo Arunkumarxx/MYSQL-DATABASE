a0cd349d2f62aadceba07304b6dd561a5d5eb32c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-15 16:45:23 +0530

    modified

--- MYSQL MISC CONCEPTS/SUBQUERY/SUBQUERY.sql
+++ MYSQL MISC CONCEPTS/SUBQUERY/SUBQUERY.sql
@@ -2,5 +2,12 @@ select * from userlist
 where id=any(select id from customers);
 
 /*
-
++------+------+
+| id   | name |
++------+------+
+|    1 | arun |
++------+------+
+1 row in set (0.00 sec)
  */
+
+ 
