01e9b7b90f8b2e12f23c1714c8f81f98d4c8aa95
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-15 12:04:33 +0530

    modified

--- MYSQL REGULAR EXPRESSIONS/REGEXP_REPLACE() FUNCTION/REGEXP_REPLACE() FUNCTION.sql
+++ MYSQL REGULAR EXPRESSIONS/REGEXP_REPLACE() FUNCTION/REGEXP_REPLACE() FUNCTION.sql
@@ -1,4 +1,5 @@
-update customers
-set name ='bank of baroda'
-where regexp_replace(name,'^b','i');
+UPDATE customers
+SET name = 'bank of baroda'
+WHERE REGEXP_LIKE(name, '^b', 'i');
+
 select * from customers;
