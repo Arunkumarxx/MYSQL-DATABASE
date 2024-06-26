a921ed9db041d1434981af1e23a6dcab73f63033
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-27 04:03:19 +0530

    modified

--- MYSQL MISC CONCEPTS/WILDCARDS/WILDCARDS.sql
+++ MYSQL MISC CONCEPTS/WILDCARDS/WILDCARDS.sql
@@ -1,5 +1,14 @@
 select * from customers where age like '_5%';
 
 /*
-
++----+------------+------+
+| id | name       | age  |
++----+------------+------+
+|  1 | Arun kumar |   25 |
+|  6 | Oscar      |   55 |
++----+------------+------+
+2 rows in set (0.00 sec)
  */
+
+ select * from customers
+  where age 
