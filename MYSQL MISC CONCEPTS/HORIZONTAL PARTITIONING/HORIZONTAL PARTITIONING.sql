cddc56877d38d483b2fe50c543294302db3a879a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-27 03:17:34 +0530

    modified

--- MYSQL MISC CONCEPTS/HORIZONTAL PARTITIONING/HORIZONTAL PARTITIONING.sql
+++ MYSQL MISC CONCEPTS/HORIZONTAL PARTITIONING/HORIZONTAL PARTITIONING.sql
@@ -8,6 +8,6 @@ CREATE TABLE employees (
 partition by list(salary)(
     partition low values less than (55000,60000),
     partition mid values less than (60001,70000),
-    partition high values less than (70001,000)
+    partition high values less than (70001,80000)
     )
 ;
