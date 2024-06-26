bb6b907206a58ecaa99de8ce227b123a637ca169
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-27 04:03:46 +0530

    modified

--- MYSQL MISC CONCEPTS/WILDCARDS/WILDCARDS.sql
+++ MYSQL MISC CONCEPTS/WILDCARDS/WILDCARDS.sql
@@ -12,3 +12,17 @@ select * from customers where age like '_5%';
 
  select * from customers
   where age not like '%5';
+
+/*
+ +----+----------------+------+
+| id | name           | age  |
++----+----------------+------+
+|  2 | bank of baroda |   32 |
+|  3 | Charlie        |   29 |
+|  4 | David          |   37 |
+|  5 | Eve            |   41 |
+|  7 | M@rk!          |   33 |
+|  8 | Hexa1F2        |   28 |
++----+----------------+------+
+6 rows in set (0.00 sec)
+ */
