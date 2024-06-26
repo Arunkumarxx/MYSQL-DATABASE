6bd91b0e1e98a00207098394efc812bb650935c8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-27 02:38:11 +0530

    modified

--- MYSQL MISC CONCEPTS/COMMON TABLE EXPRESSIONS/COMMON TABLE EXPRESSIONS.sql
+++ MYSQL MISC CONCEPTS/COMMON TABLE EXPRESSIONS/COMMON TABLE EXPRESSIONS.sql
@@ -1,7 +1,10 @@
-CREATE TABLE employees (
-    employee_id INT PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
+CREATE TABLE employees
+(
+    employee_id   INT PRIMARY KEY,
+    first_name    VARCHAR(50),
+    last_name     VARCHAR(50),
     department_id INT,
-    salary DECIMAL(10, 2)
+    salary        DECIMAL(10, 2)
+);
+
 
