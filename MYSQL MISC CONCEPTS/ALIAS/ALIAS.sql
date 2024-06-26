9a88725233b5610454c8aae04701a8be2c0388bb
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-27 04:08:24 +0530

    modified

--- MYSQL MISC CONCEPTS/ALIAS/ALIAS.sql
+++ MYSQL MISC CONCEPTS/ALIAS/ALIAS.sql
@@ -1,5 +1,17 @@
 select id as some from customers;
 
 /*
-
++------+
+| some |
++------+
+|    1 |
+|    2 |
+|    3 |
+|    4 |
+|    5 |
+|    6 |
+|    7 |
+|    8 |
++------+
+8 rows in set (0.00 sec)
  */
