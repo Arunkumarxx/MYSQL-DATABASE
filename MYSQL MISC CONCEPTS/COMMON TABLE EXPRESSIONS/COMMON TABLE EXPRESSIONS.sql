7c6f85bd3d52dc0921788f6f8396a7665437ac2b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-27 02:37:42 +0530

    modified

--- MYSQL MISC CONCEPTS/COMMON TABLE EXPRESSIONS/COMMON TABLE EXPRESSIONS.sql
+++ MYSQL MISC CONCEPTS/COMMON TABLE EXPRESSIONS/COMMON TABLE EXPRESSIONS.sql
@@ -1,2 +1,7 @@
-with ageAround as  ()
-select * from customers;
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    department_id INT,
+    salary DECIMAL(10, 2)
+);
