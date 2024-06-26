28f411efc792ee68573ce253e78039aa503af9e5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-27 03:35:02 +0530

    modified

--- MYSQL MISC CONCEPTS/STORED FUNCTIONS/STORED FUNCTIONS.sql
+++ MYSQL MISC CONCEPTS/STORED FUNCTIONS/STORED FUNCTIONS.sql
@@ -2,6 +2,7 @@
 create function some(age int )returns int
 begin
     declare arun varchar(255);
-    select employees.first_name into arun from employees where age>25 order by ;
+    select employees.first_name into arun from employees
+     where age>25 order by age asc limit 1;
 end //
 delimiter ;
