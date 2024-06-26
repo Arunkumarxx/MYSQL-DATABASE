7f6b7625a5e21a4fb2ce0cb63eb4192257269f94
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-27 03:13:42 +0530

    modified

---
+++ MYSQL MISC CONCEPTS/HORIZONTAL PARTITIONING/HORIZONTAL PARTITIONING.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    department_id INT,
+    salary DECIMAL(10, 2)
+)
+PARTITION BY KEY (department_id)
+PARTITIONS 4;
