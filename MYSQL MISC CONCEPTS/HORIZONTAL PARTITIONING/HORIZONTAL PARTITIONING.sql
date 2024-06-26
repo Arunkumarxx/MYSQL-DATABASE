b4fd831f75f79b4b500f622cf3b9b71016973c1a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-06-27 03:17:49 +0530

    modified

--- MYSQL MISC CONCEPTS/HORIZONTAL PARTITIONING/HORIZONTAL PARTITIONING.sql
+++ MYSQL MISC CONCEPTS/HORIZONTAL PARTITIONING/HORIZONTAL PARTITIONING.sql
@@ -9,5 +9,5 @@ partition by list(salary)(
     partition low values less than (55000,60000),
     partition mid values less than (60001,70000),
     partition high values less than (70001,80001)
-    )
-;
+    );
+
